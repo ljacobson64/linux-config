a7ec036260204f0e61b40f4cbcf65aca67f0da0a
Lucas John Jacobson <ljacobson64@gmail.com>
2018-04-19 16:03:35 -0500

    Split packlists by ubuntu version

--- apt-update.sh
+++ apt-update.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [[ `lsb_release -sd` == *"Xenial"* ]]; then
+  packlists="packlist-xenial"
+elif [[ `lsb_release -sd` == *"Bionic"* ]]; then
+  packlists="packlist-xenial packlist-bionic"
+fi
+if [[ `hostname` == "mcnp"* ]]; then
+  packlists+=" packlist-bionic-slurm"
+fi
+
 apt -y update
 apt -y upgrade
 apt -y dist-upgrade
-apt -y install `cat packlist`
-if [[ "${HOSTNAME}" == "mcnp"* ]]; then
-  apt -y install `cat packlist-phoenix`
-fi
+apt -y install `cat ${packlists}`
 apt -y autoremove
