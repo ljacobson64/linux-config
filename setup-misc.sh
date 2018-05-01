138dd6a69e5e55c82170fc9cc5005ce3a21543d9
Lucas John Jacobson <ljacobson64@gmail.com>
2018-05-01 12:29:17 -0500

    Add misc setup script

---
+++ setup-misc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -s /usr/lib/x86_64-linux-gnu/libieee1284.so.3 /usr/lib/libieee.so
+ln -s /usr/lib/x86_64-linux-gnu/libpmi2.so /usr/lib/libpmi2.so
+ln -s /usr/include/slurm-wlm/pmi2.h /usr/include/pmi2.h
