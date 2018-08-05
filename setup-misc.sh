ae355a3a498879b95f055ab75e207a8a81910828
Lucas John Jacobson <ljacobson64@gmail.com>
2018-08-05 11:01:02 -0500

    Symlink libieee.a

--- setup-misc.sh
+++ setup-misc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ln -s /usr/lib/x86_64-linux-gnu/libieee1284.so.3 /usr/lib/libieee.so
+ln -s /usr/lib/x86_64-linux-gnu/libieee1284.a /usr/lib/libieee.a
 ln -s /usr/lib/x86_64-linux-gnu/libpmi2.so /usr/lib/libpmi2.so
 ln -s /usr/include/slurm-wlm/pmi2.h /usr/include/pmi2.h
