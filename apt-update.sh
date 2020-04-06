203200dca50ab02eda2dff40cd472ada8c4a0b12
Lucas John Jacobson <ljacobson64@gmail.com>
2020-04-06 17:14:17 -0500

    Ubuntu 20.04 updates

--- apt-update.sh
+++ apt-update.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [[ `lsb_release -sd` == *"18.04"* ]]; then
+  aptlist=aptlist-18.04
+elif [[ `lsb_release -sd` == *"Focal"* ]]; then
+  aptlist=aptlist-20.04
+fi
+
 apt -y update
 apt -y upgrade
 apt -y dist-upgrade
-apt -y install `cat aptlist`
+apt -y install `cat ${aptlist}`
 apt -y autoremove
