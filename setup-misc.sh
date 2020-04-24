be6aaa7de8d2d0983deb79585a513bc92b8a7123
Lucas John Jacobson <ljacobson64@gmail.com>
2020-04-24 10:11:33 -0500

    Remove setup-misc.sh

--- setup-misc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ln -s /usr/lib/x86_64-linux-gnu/libieee1284.so.3 /usr/lib/libieee.so
-ln -s /usr/lib/x86_64-linux-gnu/libieee1284.a /usr/lib/libieee.a
-ln -s /usr/lib/x86_64-linux-gnu/libpmi2.so /usr/lib/libpmi2.so
-ln -s /usr/include/slurm-wlm/pmi2.h /usr/include/pmi2.h
