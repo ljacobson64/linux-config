b47b8c6c39443bf4603e83c88f519cbeac6b8402
Lucas John Jacobson <ljacobson64@gmail.com>
2022-06-03 14:52:07 -0500

    Add 22.04

--- apt-update.sh
+++ apt-update.sh
@@ -4,10 +4,12 @@ if [[ `lsb_release -sd` == *"18.04"* ]]; then
   aptlist=aptlist-18.04
 elif [[ `lsb_release -sd` == *"20.04"* ]]; then
   aptlist=aptlist-20.04
+elif [[ `lsb_release -sd` == *"22.24"* ]]; then
+  aptlist=aptlist-22.04
 fi
 
 apt -y update
 apt -y upgrade
 apt -y dist-upgrade
-apt -y install `cat ${aptlist}`
+apt -y install $(cat ${aptlist})
 apt -y autoremove
